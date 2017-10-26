2bd8bf1b1102458f95def2123acc980f7fdbcc6d
han <350748650@qq.com>
2017-10-26 20:47:54 +0800

    第二章内容添加

--- basic_learningnote/python程序设计(第2版)_chapter2_python_sequence.py
+++ basic_learningnote/python程序设计(第2版)_chapter2_python_sequence.py
@@ -258,3 +258,183 @@ vec = [[1, 2, 3], [4, 5, 6],[7, 8, 9]]
 >>>[num for elem in vec for num in elem]
 [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+(2)不符合条件的元素：在列表推导式中可以使用if子句来筛选，只在结果列表中保留符合条件的元素。
+#列出当前文件夹下所有Python源文件：
+>>>import os
+>>>[filename for filename in os.listdir('.') if filename.endswith('.py')]     #os.listdir() 方法用于返回指定的文件夹包含的文件或文件夹的名字的列表
+#从列表中选择符合条件的元素组成新的列表：
+>>>alist = [-1, -4, 6, 7.5]
+>>>[i for i in alist if i > 0]
+#已知一个包含成绩的字典，计算最高最低和平均，并查找所有最高分同学
+>>>scores = {"Alice": 25, "han": 100, "zhou": 100, "dong": 60}
+>>>highest = max(scores.values())
+>>>lowest = min(scores.values())
+>>>average = sum(scores.values()) / len(scores)
+>>>highest_person = [name for name,score in scores.items() if score == highest]
+
+(3)在列表推导式中使用多个循环，实现多序列元素的任意组合，并且可以结合条件语句过滤特定元素
+>>>[(x, y) for x in range(2) for y in range(2)]
+[(0, 0), (0, 1), (1, 0), (1, 1)]
+>>>[(x, y) for x in range(2) for y in range(2) if x != y]
+[(0, 1), (1, 0)]
+
+(4)使用列表推导式实现矩阵转置
+>>>matrix = [[1, 2, 3], [4, 5, 6]]
+>>>[[row[i] for row in matrix] for i in range(3)]
+[[1, 4], [2, 5], [3, 6]]
+
+#使用内置函数zip()和list()来实现矩阵转置
+>>>list(zip(*matrix))
+
+(5)列表推导式可以使用函数或者复杂表达式
+def f(v):
+    if v % 2 == 0:
+        v = v ** 2
+    else:
+        v += 1
+    return v
+>>>print([f(v) for v in [1, 2, 3, -1] if v > 0])
+[2, 4, 4]
+>>>print([v ** 2 if v % 2 == 0 else v + 1 for v in [1, 2, 3, -1] if v > 0])
+[2, 4, 4]
+
+(6)列表推导式支持文件对象迭代
+>>>fp = open('C:\test.txt', 'r')
+>>>print([line for line in fp])
+>>>fp.close()
+
+(7)使用列表推导式生成100以内的所有素数
+import math
+>>>[p for p in range(2, 100) if 0 not in [p % d for d in range(2, int(math.sqrt(p)) + 1)]]
+[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+
+12.使用列表实现向量运算
+>>>[1, 2] * 2
+[1, 2, 1, 2]
+python列表不支持与整数的加减除运算，也不支持列表之间的减乘除运算。列表的加法表示列表元素的合并，是生成了新的列表，而不是有向量意义的加法。
+>>>[1, 2] + [3]
+[1, 2, 3]
+
+13.tuple 属于不可变序列，不可修改元素的值，也无法增加或者删除元素，如果确实要修改，只能再创建一个新的元组
+
+14.元组的创建与删除：
+(1)=赋值,如果要创建只包含一个元素的元组，只把元素放在括号里是不行的，要在后面加一个逗号，创建多个元素的元组没有这个限制。
+>>>a_tuple = ('a',) #len(a_tuple) == 1
+>>>a = (1)          #则 a == 1 不是元组，而是数值
+>>>x = ()           #空元组
+>>>a = 1, 2         #a == (1, 2)是元组
+(2)tuple()函数可以将其他类型的序列转换为元组
+>>>print(tuple('abc'))
+('a', 'b', 'c')
+>>>tuple(list)      #tuple()可以转换list为tuple，反之不行
+元组只能使用del命令删除整个元组对象，不能只删除其中的元素
+
+15.元组与列表的区别
+元组没有append()/extend()/insert()等方法，无法向元组中添加元素，修改元素，也没有remove()/pop()
+元组也支持切片操作，但是只能通过切片访问元组中的元素，不能修改和删减元素
+元组的访问和处理速度比列表快，元组可以用作字典的键，因为不可变，而列表不可以
+虽然元组不可变，但是如果元组中有list，改变list中的元素还是可以的：
+>>>x = ([1, 2], 3)
+>>>x[0][0] = 0
+>>>x
+([0, 2], 3)
+>>>x[0].append(0)
+>>>x
+([0, 2, 0], 3)
+>>>x[0] = x[0] + [2]
+Traceback (most recent call last):
+  File "<input>", line 1, in <module>
+TypeError: 'tuple' object does not support item assignment
+
+16.序列解包：可以用非常简洁的形式完成复杂的功能，提高代码可读性
+(1)用序列解包功能对多个变量同时赋值
+>>>x, y, z = 1, 2, 3
+>>>print(x, y, z)
+1 2 3
+(2)序列解包可以作用于列表和字典，对字典使用，默认对于键操作，若要操作键值对，应该用.items()方法，若对值操作，用.values()方法，
+>>>a = [1, 2, 3]
+>>>b, c, d = a
+>>>s = {'a': 1, 'b': 2, 'c': 3}
+>>>b, c, d = s.items()
+>>>b
+('a', 1)
+(3)序列解包方便遍历多个序列
+>>>keys = ['a', 'b', 'c']
+>>>values = [1, 2, 3]
+>>>for k, v in zip(keys, values):
+     print(k, v)
+a 1
+b 2
+c 3
+
+(4)序列解包支持以下用法：
+>>>print(*[1, 2],2)
+1 2 2
+>>>def demo(a, b):
+     print(a, b)
+>>>demo(**{'a':1, 'b': 2})
+1 2
+>>>*range(4), 4
+(0, 1, 2, 3, 4)
+>>>[*range(4), 4]
+[0, 1, 2, 3, 4]
+>>>{*range(3), *(1,)}
+{0, 1, 2}
+
+17.生成器推导式：生成器推导式使用圆括号而列表推导式使用方括号。生成器推导式的结果是个生成器对象，不是列表也不是元组，使用时在转化为列表货源足，也可以用
+生成器对象的next()(p2)方法，或者__next__()方法(p3)进行遍历，或者直接作为迭代器对象使用，但是当所有元素访问结束以后，需要重新访问其中的元素，必须重新创建
+生成器对象。
+>>>g = ((i + 2) ** 2 for i in range(10))
+>>>g
+<generator object <genexpr> at 0x00000000035286D0>
+>>>tuple(g)
+(4, 9, 16, 25, 36, 49, 64, 81, 100, 121)
+>>>g = ((i + 2) ** 2 for i in range(10))
+>>>list(g)
+[4, 9, 16, 25, 36, 49, 64, 81, 100, 121]
+>>>g = ((i + 2) ** 2 for i in range(10))
+>>>g.__next__()
+4
+>>>g.__next__()
+9
+
+18.字典：是键-值对的无序可变序列，字典中的每个元素包含键值两部分，键值用冒号分开，相邻元素用逗号分开，{}里面放置
+字典中的键可以使Python中任意不可变数据，但是不能使用可变对象，如列表等，字典中的键不可以重复，而值可以重复
+用内置函数globals()返回和查看包含当前作用域内所有全局变量和值的字典，locals()返回包含当前作用域内所有局部变量和值的字典
+a = (1, 2)
+b = 'hello'
+def demo():
+    a = 0
+    print('locals: ', locals())
+    print('globals: ', globals())
+demo()
+locals: {'a': 0}
+globals: {'__builtins__': {'__name__': 'builtins',
+                           '__doc__': "Built-in functions, exceptions, and other objects.\n\nNoteworthy: None is the `nil' object; Ellipsis represents `...' in slices.",
+                           '__package__': '', '__loader__': <}
+
+19.字典创建与删除
+(1)=赋值
+>>>a_dict = {'good': 100}
+>>>a_dict
+{'good': 100}
+(2)使用内置函数dict()通过已有数据快速创建字典
+>>>keys = ['han', 'zhou']
+>>>values = [100, 99]
+>>>dict1 = dict(zip(keys, values))
+>>>dict1
+{'han': 100, 'zhou': 99}
+#空字典
+>>>x = dict()
+>>>x = {}
+(3)dict()函数给定键值对创建字典
+>>>dict2 = dict(han = 100, zhou = 99)
+>>>dict2
+{'han': 100, 'zhou': 99}
+(4)给定内容为键，创建值为空的字典
+>>>dict3 = dict.fromkeys(['han', 'zhou'])
+>>>dict3
+{'han': None, 'zhou': None}
+
+20.字典元素的读取
+
